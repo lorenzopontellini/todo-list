311c0b929a0ca50ae2b12284636b1d8905927faa
Lorenzo Pontellini <lorenzo.pontellini@mapsgroup.it>
2025-10-16 00:19:07 +0200

    add schema for swagger

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,14 +1,17 @@
 import { zodToJsonSchema } from "zod-to-json-schema";
-import { errorResponseSchema, loginSchemaRequest, loginSchemaResponse, registerSchemaRequest } from "../schema/auth.schema";
+import { errorResponseSchema, loginSchemaRequest, loginSchemaResponse, registerSchemaRequest, userSchema } from "../schema/auth.schema";
 import { userUpdateNameSchemaRequest } from "../schema/user.schema";
 
+export const ErrorResponseSchema = zodToJsonSchema(errorResponseSchema, "ErrorResponseSchema");
+
+export const UserSchema = zodToJsonSchema(userSchema, "UserSchema");
 export const RegisterSchemaRequest = zodToJsonSchema(registerSchemaRequest, "RegisterSchemaRequest");
-// export const RegisterSchemaResponse = zodToJsonSchema(registerSchemaResponse, "RegisterSchemaResponse");
 
 export const LoginSchemaRequest = zodToJsonSchema(loginSchemaRequest, "LoginSchemaRequest");
 export const LoginSchemaResponse = zodToJsonSchema(loginSchemaResponse, "LoginSchemaResponse");
 
-export const UserUpdateNameSchemaRequest = zodToJsonSchema(userUpdateNameSchemaRequest, "UserUpdateNameSchemaRequest");
-// export const UserSchema = zodToJsonSchema(userSchema, "UserSchema");
+export const createTaskSchema = zodToJsonSchema(userSchema, "CreateTaskSchema");
+export const TaskSchema = zodToJsonSchema(userSchema, "TaskSchema");
+export const UpdateTaskSchema = zodToJsonSchema(userSchema, "UpdateTaskSchema");
 
-export const ErrorResponseSchema = zodToJsonSchema(errorResponseSchema, "ErrorResponseSchema");
+export const UserUpdateNameSchemaRequest = zodToJsonSchema(userUpdateNameSchemaRequest, "UserUpdateNameSchemaRequest");
